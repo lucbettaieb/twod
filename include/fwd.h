2045c18f0a2ef1f89aef636402408b92b67a4751
Brian Cairl <bcairl@fetchrobotics.com>
2019-01-21 00:16:19 -0800

    Removes some excessive forward declarations

--- include/fwd.h
+++ include/fwd.h
@@ -17,17 +17,10 @@ template<typename CoordT> struct Coordinates;
 template<typename GridT> struct Tile;
 template<typename Derived, typename BoundsT> class GridBase;
 template<typename Derived> class BoundsBase;
-
 template<typename GridT> class GridTraits;
-
 template<typename ViewT> class ColViewIterator;
 template<typename ViewT> class RowViewIterator;
-
 template<typename ParentT, typename BoundsT> class View;
-
-template<typename CellT, typename AllocatorT> class Grid;
-template<typename CellT, int Height, int Width> class FixedGrid;
-template<typename CellT, int Height, int Width, int TileHeight, int TileWidth> class FixedTiledGrid;
 
 }  // namespace twod
 
