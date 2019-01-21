56563ee12eb406c2235f5ca8905d899a9297a521
Brian Cairl <bcairl@fetchrobotics.com>
2019-01-21 01:15:08 -0800

    Splits on TiledGrid to its own header
    - Also splits out tiled grid tests from grid.cpp unit tests

--- test/grid.cpp
+++ test/grid.cpp
@@ -319,58 +319,6 @@ TEST(FixedMappedGrid, FromArrayAssignGrid)
 }
 
 
-TEST(FixedTiledGrid, UniformInitialValueConstructor)
-{
-  FixedTiledGrid<int, 20, 20, 10, 10> grid{5};
-
-  for (const auto& v : grid)
-  {
-    ASSERT_EQ(v, 5);
-  }
-}
-
-
-TEST(FixedTiledGrid, SingleTileAssign)
-{
-  FixedTiledGrid<int, 20, 20, 20, 20> grid{5};
-
-  grid[Indices{5, 5}] = 6;
-
-  EXPECT_EQ((grid[Indices{5, 5}]), 6);
-}
-
-
-TEST(FixedTiledGrid, Assign)
-{
-  FixedTiledGrid<int, 20, 20, 5, 5> grid{5};
-
-  grid[Indices{5, 5}] = 6;
-  grid[Indices{18, 19}] = 9;
-
-  ASSERT_TRUE((grid.mask()[Indices{1, 1}]));
-  ASSERT_TRUE((grid.mask()[Indices{3, 3}]));
-  ASSERT_EQ(grid.active(), 2UL);
-
-  EXPECT_EQ((grid[Indices{5, 5}]), 6);
-  EXPECT_EQ((grid[Indices{18, 19}]), 9);
-}
-
-
-TEST(FixedTiledGrid, AssignGrid)
-{
-  FixedTiledGrid<int, 20, 20, 5, 5> grid{1};
-
-  grid.view(FixedOriginExtentsBounds<1, 1, 2, 2>{}) = FixedGrid<int, 2, 2>{5};
-
-  EXPECT_EQ((grid[Indices{0, 0}]), 1);
-  EXPECT_EQ((grid[Indices{1, 1}]), 5);
-  EXPECT_EQ((grid[Indices{1, 2}]), 5);
-  EXPECT_EQ((grid[Indices{2, 1}]), 5);
-  EXPECT_EQ((grid[Indices{2, 2}]), 5);
-  EXPECT_EQ((grid[Indices{3, 3}]), 1);
-}
-
-
 // //
 // // ASSIGNMENT "STRESS" TESTS
 // //
@@ -395,19 +343,6 @@ TEST(FixedGrid, AssignIterated)
   {
     c = 2;
   }
-}
-
-
-TEST(FixedTiledGrid, AssignIterated)
-{
-  FixedTiledGrid<int, 2000, 2000, 50, 50> grid{1};
-
-  for (auto& c : grid)
-  {
-    c = 2;
-  }
-
-  ASSERT_EQ(grid.active(), (FixedTiledGrid<int, 2000, 2000, 50, 50>::TileCount));
 }
 
 
